072766a3e9551458e92ca9ad976cb1b7b6e030cf
zb-rafael <zbrafael@carc.pshs.edu.ph>
2026-02-08 16:56:19 +0800

    Enhance user interaction with name and age inputs
    Added input prompts for name and age, and included error handling for age input.

--- main.py
+++ main.py
@@ -2,15 +2,22 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+print("Program started")
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hello, GitHub!")  # Press Ctrl+F8 to toggle the breakpoint.
+name = input("Enter your name: ")
+print("Hello", name)
 
-
+try:
+    age = int(input("Enter your age: "))
+    print("You are", age)
+except ValueError:
+    print("Invalid Input")
+    
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Pycharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
