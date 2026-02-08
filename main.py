8c54e3a346c4093317762553f806bcc579801066
zb-rafael <zbrafael@carc.pshs.edu.ph>
2026-02-08 17:28:08 +0800

    Added a percentage calculator
    Added age input handling and a decimal to percentage calculator.

--- main.py
+++ main.py
@@ -2,29 +2,39 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-print("Program started")
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hello, GitHub!")  # Press Ctrl+F8 to toggle the breakpoint.
+
+print("Program started")
+
+# Greets User
 name = input("Enter your name: ")
 print("Hello", name)
 
+# Age Function
 try:
     age = int(input("Enter your age: "))
     print("You are", age)
 except ValueError:
     print("Invalid Input")
 
+# Addition Calculator
 def add(a, b):
     return a + b
     
-num1 = int(input("Enter first number:")
-num2 = int(input("Enter second number:")
-           
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
 result = add(num1, num2)
 print("The sum is", result)
-    
+
+# Decimal to Percentage Calculator
+decimal = float(input("Enter a decimal number"))
+percentage = decimal * 100
+print(f"{decimal} as a percentage is {percentage}% ")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Pycharm')
